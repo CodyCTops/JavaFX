6dba05d20d3c923f5ae83a9f3edb1087d1873af2
Cody Tops <codytops1@gmail.com>
2024-06-03 14:15:34 -0400

    first commit

---
+++ MyProgram.java
@@ -0,0 +1,20 @@
+//Beginings of FoodInflationApp
+
+package javaFx;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MyProgram extends Application {
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,5 @@
+class Test {
+    public static void main(Stringsp[] args) {
+        System.out.println("Hello World! Your Code Works!")
+    }
+}
